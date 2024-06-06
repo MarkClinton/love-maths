9d59465bf6f856827ea7aae6a3fc26e4dc067385
Mark <markc9309@yahoo.com>
2024-06-06 22:21:30 +0100

    Added code to increase scorrect and incorrect score and display it to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,8 +47,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`Awww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -71,13 +73,20 @@ function calculateCorrectAnswer() {
         throw `Unimplmented operator ${operator}. Aborting!`;
     }
 }
-
+/**
+ * Gets the current score from the dom and adds 1
+ */
 function incrementScore() {
-
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
+/**
+ * Gets the current incorrect score from the dom and adds 1
+ */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
